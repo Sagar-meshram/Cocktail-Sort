c6df3a99e33921352b5512ed22c9d64da8ea57af
Sagar-Meshram <sagar.meshram@walchandsangli.ac.in>
2024-10-17 18:40:51 +0530

    Add Cocktail Sort implementation

---
+++ cocktail.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void cocktailSort(vector<int>& arr) {
+    bool swapped = true;
+    int start = 0;
+    int end = arr.size() - 1;
+
+    while (swapped) {
+        swapped = false;
+
+        // Forward pass
+        for (int i = start; i < end; ++i) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) break; // Array is sorted
+
+        swapped = false;
+        --end;
+
+        // Backward pass
+        for (int i = end - 1; i >= start; --i) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+
+        ++start;
+    }
+}
+
+int main() {
+    vector<int> arr = {5, 2, 9, 1, 5, 6};
+    cocktailSort(arr);
+
+    cout << "Sorted array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
